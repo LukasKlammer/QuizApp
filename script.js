e3e4b7b29a33c2d2f20a2ff252145f3c69863bc6
LukasKlammer <klammer.lukas@hotmail.com>
2022-03-30 16:48:25 +0200

    implement logic of answers

--- script.js
+++ script.js
@@ -36,6 +36,20 @@ function showQuestion() {
     }
 
 
+}
 
+function answer(selectedAnswer) {
+    let question = questions[currentQuestion] // holt die aktuelle Frage in aus dem JSON und schreibt sie in die Variable question
+    let rightAnswer = question["right answer"]; // aus der aktuellen Frage die richtige Antwort auslesen und in die Variable rightAnswer geben (Antwort ist eine Zahl)
+    // let selectedAnswerNumber = selectedAnswer.slice(-1);  // Alternative: damit lesen wir das letzte Zeichen aus dem übergebenen String raus
 
+    let rightAnswerAsText = `answer_${rightAnswer}`;
+
+    if (selectedAnswer == rightAnswerAsText) { // wenn die beiden strings übereinstimmen (z. B. answer_1 == answer_1) --> mache was
+        document.getElementById(selectedAnswer).parentNode.classList.add('bg-success');
+    }
+    else if (selectedAnswer != rightAnswerAsText) {
+        document.getElementById(selectedAnswer).parentNode.classList.add('bg-danger');
+        document.getElementById(rightAnswerAsText).parentNode.classList.add('bg-success');
+    }
 }
