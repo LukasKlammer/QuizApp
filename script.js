d33a65ce3e93895d94e55c89069fd604403d7fd7
LukasKlammer <klammer.lukas@hotmail.com>
2022-04-05 20:00:00 +0200

    add questions percentage and proportionality

--- script.js
+++ script.js
@@ -56,12 +56,6 @@ function init() {
 }
 
 
-/**Öffnet das Quiz */
-function openQuiz() {
-    showQuestionScreen();
-}
-
-
 /**rendert die Fragen, sofern nicht bereits die letzte Frage aufgerufen wurde */
 function showQuestion() {
     let question = selectedQuiz[currentQuestion]; // aktuelle Frage aus dem JSON holen und in ein array speichern
@@ -70,40 +64,46 @@ function showQuestion() {
         showEndScreen(); // am Ende des Quizzes End-Bildschirm zeigen
     }
     else { // wenn noch nicht am Ende angelangt kommt nächste Frage
-        renderQuestionAndNumber(question); // rendert nächste Frage
-        renderProgressBar(question);
-        for (let i = 1; i < 5; i++) {
-            resetAnswerButtons(i); // setzt die Farben der Antwortmöglichkeiten zurück
-            renderSingleAnswer(question, i);  // rendert die Antwortmöglichkeiten
-        }
+        renderEntireQuestion(question); // rendert nächste Frage
     }
 }
 
 
 function answer(selectedAnswer) {
-    // let selectedAnswerNumber = selectedAnswer.slice(-1);  // Alternative: damit lesen wir das letzte Zeichen aus dem übergebenen String raus
     let question = selectedQuiz[currentQuestion] // holt die aktuelle Frage in aus dem JSON und schreibt sie in die Variable question
-    let rightAnswer = question["right answer"]; // aus der aktuellen Frage die richtige Antwort auslesen und in die Variable rightAnswer geben (Antwort ist eine Zahl)
-    let rightAnswerAsText = `answer_${rightAnswer}`;
+    let rightAnswerAsText = `answer_${question["right answer"]}`;
 
-    if (selectedAnswer == rightAnswerAsText) { // wenn die beiden strings übereinstimmen (z. B. answer_1 == answer_1) --> mache was
-        document.getElementById(selectedAnswer).parentNode.classList.add('bg-success');
-        AUDIO_SUCCESS.play(); // success Ton abspielen
-        score++; // wenn richtige Antwort, erhöhe die Punktzahl um 1
+    if (selectedAnswer == rightAnswerAsText) { // wenn die beiden strings übereinstimmen (z. B. answer_1 == answer_1)
+        reactionRightAnswer(selectedAnswer);
     }
     else if (selectedAnswer != rightAnswerAsText) {
-        document.getElementById(selectedAnswer).parentNode.classList.add('bg-danger');
-        document.getElementById(rightAnswerAsText).parentNode.classList.add('bg-success');
-        AUDIO_FAIL.play(); // fail Ton abspielen
+        reactionWrongAnswer(selectedAnswer, rightAnswerAsText);
     }
+
+    lockAnswers(); // sperrt die weiteren Antwortmöglichkeiten
     enableNextQuestion();
 }
 
 
-function nextQuestion() {
+function reactionRightAnswer(selectedAnswer) {
+    document.getElementById(selectedAnswer).parentNode.classList.add('bg-success');
+    AUDIO_SUCCESS.play();
+    score++;
+}
+
+
+function reactionWrongAnswer(selectedAnswer, rightAnswerAsText) {
+    document.getElementById(selectedAnswer).parentNode.classList.add('bg-danger');
+    document.getElementById(rightAnswerAsText).parentNode.classList.add('bg-success');
+    AUDIO_FAIL.play();
+}
+
+
+function showNextQuestion() {
     disableNextQuestion();
     currentQuestion++; // Variable um 1 erhöhen, gibt die Nummer der aktuellen Frage an
     showQuestion(selectedQuiz);
+    unLockAnswers();
 }
 
 
@@ -112,9 +112,14 @@ function resetAnswerButtons(i) {
 }
 
 
-function renderQuestionAndNumber(question) {
+function renderEntireQuestion(question) {
     document.getElementById('current-question').innerHTML = currentQuestion + 1; // rendert die Nummer der aktuellen Frage; +1 braucht es, weil array mit 0 beginnt
     document.getElementById('question-text').innerHTML = question['question']; // rendert die Frage
+    renderProgressBar(question); // Fortschrittsbalken
+    for (let i = 1; i < 5; i++) {
+        resetAnswerButtons(i); // setzt die Farben der Antwortmöglichkeiten zurück
+        renderSingleAnswer(question, i);  // rendert die Antwortmöglichkeiten
+    }
 }
 
 
@@ -123,38 +128,48 @@ function renderSingleAnswer(question, i) {
 }
 
 
-/**aktiviert den button, der zur nächsten Frage führt */
 function enableNextQuestion() {
     document.getElementById('next-button').disabled = false; // button aktivieren, damit wir weiter zur nächsten Frage kommen
 }
 
 
-/**deaktiviert den button, der zur nächsten Frage führt */
 function disableNextQuestion() {
     document.getElementById('next-button').disabled = true; // button deaktivieren
 }
 
-
-/**zeigt den End-Screen mit den Ergebnissen und der Möglichkeit zum Neustart */
-function showEndScreen() {
-    document.getElementById('end-screen').classList.remove('d-none'); // Klasse d-none entfernen --> Element wird angezeigt
-    document.getElementById('question-body').classList.add('d-none'); // Fragen-Container ausblenden
-    document.getElementById('header-image').src = 'img/brain_result.png'; // ändert das Bild
-    document.getElementById('header-image').classList.add('width-50'); // verkleinert das Bild, damit es besser aussieht
-    document.getElementById('end-score').innerHTML = score + '/' + selectedQuiz.length; // zeigt an, wie viele von insgesamt wie vielen Fragen man richtig beantwortet hat
+/**disable pointer event Antwort-Karten */
+function lockAnswers() {
+    for (i = 1; i < 5; i++) {
+        document.getElementById('answer_' + i).style.pointerEvents = 'none';
+    }
 }
 
 
-/**zeigt den Screen mit den Fragen und Antworten */
+function unLockAnswers() {
+    for (i = 1; i < 5; i++) {
+        document.getElementById('answer_' + i).style.pointerEvents = 'auto';
+    }
+}
+
+
+function showEndScreen() {
+    document.getElementById('end-screen').classList.remove('d-none');
+    document.getElementById('question-body').classList.add('d-none');
+    document.getElementById('header-image').src = 'img/brain_result.png';
+    document.getElementById('header-image').classList.add('width-50');
+    document.getElementById('end-score').innerHTML = score + '/' + selectedQuiz.length;
+}
+
+
 function showQuestionScreen() {
     document.getElementById('start-screen').classList.add('d-none');
-    document.getElementById('question-body').classList.remove('d-none'); // Fragen-Container einblenden
-    document.getElementById('end-screen').classList.add('d-none'); // Klasse d-none hinzufügen --> Element wird ausgeblendet
+    document.getElementById('question-body').classList.remove('d-none');
+    document.getElementById('end-screen').classList.add('d-none');
     document.getElementById('header-image').classList.remove('width-50'); // wieder normale Größe des Bilds
-    document.getElementById('header-image').src = 'img/pencil.jpg'; // Bild wieder zurücksetzen
+    document.getElementById('header-image').src = 'img/pencil.jpg';
 }
 
-/**zeigt die Startseite */
+
 function showWelcomeScreen() {
     document.getElementById('end-screen').classList.add('d-none');
     document.getElementById('question-body').classList.add('d-none');
@@ -164,28 +179,34 @@ function showWelcomeScreen() {
 }
 
 
-/**replay: damit kann das gleiche Quiz nochmal gespielt werden */
 function replay() {
-    currentQuestion = 0; // Variablen zurücksetzen
-    score = 0;
+    resetGlobalVariables();
     showQuestionScreen(); // wieder zurück zum Container mit den Fragen
     init(); // Quiz neu starten
 }
 
 
-/**restart: damit kann die App neu gestartet werden */
 function restart() {
-    currentQuestion = 0; // Variablen zurücksetzen
-    score = 0;
+    resetGlobalVariables();
     showWelcomeScreen();
     init();
 }
 
 
-/**rendert den Verlauf der Progress-Bar */
+function resetGlobalVariables() {
+    currentQuestion = 0;
+    score = 0;
+}
+
+
 function renderProgressBar(question) {
     let percentage = ((currentQuestion + 1) * 100) / selectedQuiz.length; // Prozentsatz berechnen
-    percentage = Math.round(percentage); // Ergebnis runden mit JavaScript Funktion
+    percentage = Math.round(percentage); // Ergebnis runden
     document.getElementById('progress-bar').style = `width: ${percentage}%`; // Breite der progressbar einsetzen
     document.getElementById('progress-bar').innerHTML = percentage + ' %'; // Prozentsatz in der progressbar einsetzen
+}
+
+
+function share() {
+    alert('Diese Funktion ist noch nicht implementiert.');
 }
